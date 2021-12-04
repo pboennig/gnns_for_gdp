273be06a9638177f99f58b6fff602d756598e603
Rahul <rshiv2@stanford.edu>
2021-12-04 09:52:48 -0800

    Include node features in GNN model

--- baseline.py
+++ baseline.py
@@ -5,37 +5,47 @@ import torch.nn.functional as F
 import torch.nn as nn
 from torch_geometric.nn import GCNConv
 from torch_geometric.loader import DataLoader
+
 import csv
+import numpy as np
+import matplotlib.pyplot as plt
 
 FIRST_YEAR = 1995
 LAST_YEAR = 2019
+FEATURES = ['pop', 'cpi', 'emp']
 
 def create_data(year):
     assert(year in range(FIRST_YEAR, LAST_YEAR + 1))
-    edges = pd.read_csv(f'output/X_{year}.csv')
+    edges = pd.read_csv(f'output/X_EDGE_{year}.csv')
 
     # generate map from iso_code to ids of form [0, ..., num_unique_iso_codes - 1]
     iso_codes = set(edges['i'])
     iso_codes = iso_codes.union(set(edges['j']))
     iso_code_to_id = {code : i for (i, code) in enumerate(iso_codes)}
 
+    # load in edge index
     edges['i_id'] = edges['i'].map(iso_code_to_id)
     edges['j_id'] = edges['j'].map(iso_code_to_id)
     edge_index = torch.from_numpy(edges[['i_id', 'j_id']].to_numpy()).t()
-
-
+    
+    # load in target values
     y_df = pd.read_csv(f'output/Y_{year}.csv')
     y_df['id'] = y_df['iso_code'].map(iso_code_to_id)
     y = torch.from_numpy(y_df.sort_values('id')[f'{year+1}'].to_numpy()).unsqueeze(1) # get labels as tensor
-    x = torch.ones_like(y) # dummy node features
+    
+    # load in input features
+    x_df = pd.read_csv(f'output/X_NODE_{year}.csv')
+    x_df['id'] = x_df['iso_code'].map(iso_code_to_id)
+    x = torch.from_numpy(x_df.sort_values('id').loc[:,features].to_numpy())
     return Data(x=x, edge_index=edge_index, y=y)
 
-data_list = [create_data(year) for year in range(FIRST_YEAR, LAST_YEAR-10)]
+data_list = [create_data(year) for year in range(FIRST_YEAR, LAST_YEAR)]
 
+# define model
 class GCN(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = GCNConv(1, 16)
+        self.conv1 = GCNConv(3, 16)
         self.conv2 = GCNConv(16, 16)
         self.linear = nn.Linear(16, 1)
 
@@ -49,14 +59,18 @@ class GCN(torch.nn.Module):
 
         return self.linear(x)
 
+# train model
 model = GCN().double() # needs to be double precision
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, weight_decay=5e-4)
 loader = DataLoader(data_list, batch_size=2, shuffle=True)
 
 model.train()
 losses = []
 save_interval = 100
 for epoch in range(2000):
+    if epoch % 10 == 0:
+        print(f"{round((epoch + 1)/2000 * 100, 2)}%", end='\r')
+        
     optimizer.zero_grad()
     data = next(iter(loader))
     out = model(data)
@@ -66,8 +80,15 @@ for epoch in range(2000):
     loss.backward()
     optimizer.step()
 
+# write model results to disk
 with open('results/baseline_train.csv', 'w+') as f:
     writer = csv.writer(f)
     for (i, loss) in enumerate(losses):
         writer.writerow([i * save_interval, loss])
 
+# plot results
+plt.figure()
+plt.plot(losses)
+plt.title("GNN Loss")
+plt.xlabel("Epoch (hundreds)")
+plt.show()
