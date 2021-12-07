87b002101a6d9a895ee26a7cbfe87d6f30e15f55
Peter Boennighausen <pboennig@gmail.com>
2021-12-07 04:26:02 +0000

    better bounding around scatter

--- plot.py
+++ plot.py
@@ -9,11 +9,17 @@ def loss_plot(model_type):
     loss_plot = sns.relplot(data=loss_df, x='epoch', y='loss', hue='loss_type', kind='line').set(title=f"{model_type} training trajectory")
     loss_plot.fig.savefig(f'plots/{model_type}_loss.png', dpi=400)
 
-def pred_plot(model_type):
+def pred_plot(model_type, bounding_scaling_factor=.15):
     preds_df = pd.read_csv(f'results/{model_type}_prediction.csv')
-    lim = (preds_df[['ground_truth','prediction']].min().values.min() - 1e6, preds_df[['ground_truth', 'prediction']].max().values.max())
-    preds_plot = sns.relplot(data=preds_df, x='ground_truth', y='prediction')\
-                    .set(xscale='log', yscale='log', xlabel='actual GDP', ylabel='predicted GDP', xlim=lim, ylim=lim, title=f"{model_type} prediction error")
+    # make limits nicely surround points
+    min_val = preds_df[['ground_truth','prediction']].min().values.min() * (1 - bounding_scaling_factor)
+    max_val = preds_df[['ground_truth', 'prediction']].max().values.max() * (1 + bounding_scaling_factor)
+    lim = (min_val, max_val)
+    preds_plot = sns.relplot(data=preds_df, x='ground_truth', y='prediction')
+    preds_plot.set(xscale='log', yscale='log') # large spread in values
+    preds_plot.set(xlabel='actual GDP', ylabel='predicted GDP') # label axes
+    preds_plot.set(xlim=lim, ylim=lim) # limits must be same to match intution that y=x is correct
+    preds_plot.set(title=f"{model_type} prediction error")
     preds_plot.fig.savefig(f'plots/{model_type}_prediction_error.png', dpi=400)
 
 for model_type in ['baseline', 'model']:
