880e1b710bd975c5ff272924400a85807114b87a
Peter Boennighausen <pboennig@users.noreply.github.com>
2021-12-06 16:12:33 -0800

    Split up baseline and model (#3)
    * decompose
    
    * train both models to compare
    
    Co-authored-by: Peter Boennighausen <pboennig@stanford.edu>

--- model.py
+++ model.py
@@ -3,29 +3,30 @@ import pandas as pd
 from torch_geometric.data import Data
 import torch.nn.functional as F
 import torch.nn as nn
-from torch_geometric.nn import GCNConv, GATConv
+from torch_geometric.nn import GATConv
 from torch_geometric.loader import DataLoader
-import random
 from util import *
 
 # define model
-class BaselineGDPModel(torch.nn.Module):
+class GDPModel(torch.nn.Module):
     def __init__(self, num_features=3, hidden_size=32, target_size=1):
         super().__init__()
         self.hidden_size = hidden_size
         self.num_features = num_features
         self.target_size = target_size
-        self.conv1 = GATConv(self.num_features, self.hidden_size)
-        self.conv2 = GATConv(self.hidden_size, self.hidden_size)
+        self.conv1 = GATConv(self.num_features, self.hidden_size, edge_dim = NUM_EDGE_FEATURES)
+        self.conv2 = GATConv(self.hidden_size, self.hidden_size, edge_dim = NUM_EDGE_FEATURES)
         self.linear = nn.Linear(self.hidden_size, self.target_size)
 
     def forward(self, data):
         x, edge_index, edge_attr = data.x, data.edge_index, data.edge_attr
+        # TODO: figure out if L2 normalizing edge features improve model.
+        # edge_attr = torch.nn.functional.normalize(edge_attr, p=2.0, dim=0, eps=1e-12, out=None)
 
-        x = self.conv1(x, edge_index)
+        x = self.conv1(x, edge_index, edge_attr=edge_attr)
         x = F.relu(x)
         x = F.dropout(x, training=self.training)
-        x = self.conv2(x, edge_index)
+        x = self.conv2(x, edge_index, edge_attr=edge_attr)
         x = F.dropout(x, training=self.training)
         x = F.relu(x)
 
---
+++ util.py
@@ -0,0 +1,67 @@
+
+import torch
+import pandas as pd
+from torch_geometric.data import Data
+import random
+import torch.nn.functional as F
+
+FIRST_YEAR = 1995
+LAST_YEAR = 2019
+FEATURES = ['pop', 'cpi', 'emp']
+NUM_TRAIN = 15
+NUM_VAL = 3
+NUM_TEST = 6
+NUM_EDGE_FEATURES = 10
+EDGE_FEATURES = ['f'+str(i) for i in range(NUM_EDGE_FEATURES)]
+
+
+def create_data(year):
+    '''
+    For given year, pull in node features, edge features, and edge index and 
+    save in a PyG Data object.
+    '''
+    assert(year in range(FIRST_YEAR, LAST_YEAR + 1))
+    edges = pd.read_csv(f'output/X_EDGE_{year}.csv')
+
+    # generate map from iso_code to ids of form [0, ..., num_unique_iso_codes - 1]
+    iso_codes = set(edges['i'])
+    iso_codes = iso_codes.union(set(edges['j']))
+    iso_code_to_id = {code : i for (i, code) in enumerate(iso_codes)}
+
+    # load in edge index
+    edges['i_id'] = edges['i'].map(iso_code_to_id)
+    edges['j_id'] = edges['j'].map(iso_code_to_id)
+    edge_index = torch.from_numpy(edges[['i_id', 'j_id']].to_numpy()).t()
+    edge_attr = torch.from_numpy(edges[EDGE_FEATURES].to_numpy()) #extract the features from the dataset.
+    
+    # if year == 1995:
+    #     print('edges')
+    #     print(edge_attr)
+    #     print('wut')
+    # return
+
+    # load in target values
+    y_df = pd.read_csv(f'output/Y_{year}.csv')
+    y_df['id'] = y_df['iso_code'].map(iso_code_to_id)
+    y = torch.from_numpy(y_df.sort_values('id')[f'{year+1}'].to_numpy()).unsqueeze(1) # get labels as tensor
+    
+    # load in input features
+    x_df = pd.read_csv(f'output/X_NODE_{year}.csv')
+    x_df['id'] = x_df['iso_code'].map(iso_code_to_id)
+    features = ['pop', 'cpi', 'emp']
+    x = torch.from_numpy(x_df.sort_values('id').loc[:,features].to_numpy())
+    return Data(x=x, edge_index=edge_index, edge_attr=edge_attr, y=y)
+
+def evaluate_model(model, data_val):
+    loss = 0.0
+    for data in data_val:
+        loss += F.mse_loss(model(data), data.y)
+    return loss.item()
+
+def get_data():
+    data_list = [create_data(year) for year in range(FIRST_YEAR, LAST_YEAR)]
+    random.shuffle(data_list)
+    data_train = data_list[:NUM_TRAIN]
+    data_val = data_list[NUM_TRAIN:NUM_TRAIN+NUM_VAL+1]
+    data_test = data_list[NUM_TRAIN+NUM_VAL:]
+    return (data_train, data_val, data_test)
